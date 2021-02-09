d65bfbd7c0aea955358a2fcc5768555bf8ec745c
sam_el-kamand <sam.elkamand@gmail.com>
2021-02-09 16:17:43 +1100

    Added GISTIC oncoplot

---
+++ R/mod_plot_gistic_oncoplot.R
@@ -0,0 +1,90 @@
+#' plot_gistic_oncoplot UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_plot_gistic_oncoplot_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    
+    plotOutput(ns("out_plot_gistic_oncoplot")) %>% shinycssloaders::withSpinner(proxy.height = "60px"),
+    
+    shinyWidgets::panel(
+      heading = "Options",
+      
+      #numericInput(ns("in_num_top_n"), label = "Number of Cytobands to Show", value = 5, min = , max = , step = )
+      fluidRow(
+        mod_select_maf_clinical_data_column_ui(id = ns("mod_select_clinical_feature"), multiple = TRUE, label = "Select Clinical Feature") %>% col_3(),
+        shinyWidgets::awesomeCheckbox(inputId = ns("in_check_show_tsb"), label = "Toggle Tumor Sample Barcodes", value = TRUE) %>% col_3(style = "margin-top: 30px"),
+        shinyWidgets::awesomeCheckbox(inputId = ns("in_check_sort_by_annotation"), label = "Sort by annotation", value = FALSE) %>% col_3(style = "margin-top: 30px"),
+        shinyWidgets::awesomeCheckbox(inputId = ns("in_check_remove_nonaltered_samples"), label = "Remove NonAltered Samples", value = TRUE) %>% col_3(style = "margin-top: 30px")
+      ),
+      fluidRow(
+        numericInput(ns("in_num_fontsize_samplename"), label = "Fontsize: Samples", value = 0.6, min = 0.01, step = 0.1) %>% col_3(),
+        numericInput(ns("in_num_fontsize_cytobands"), label = "Fontsize: Cytobands", value = 0.8, min = 0.01, step = 0.1) %>% col_3(),
+        numericInput(ns("in_num_fontsize_legnend"), label = "Fontsize: Legend", value = 1.2, min = 0.01, step = 0.1) %>% col_3(),
+        numericInput(ns("in_num_fontsize_annotations"), label = "Fontsize: Annotations", value = 1.2, min = 0.01, step = 0.1) %>% col_3()
+      ),
+      fluidRow(
+        numericInput(ns("in_num_mar_gene"), label = "Margin: Genes", value = 5, min = 0.01, step = 1) %>% col_3(),
+        numericInput(ns("in_num_mar_barcodes"), label = "Margin: Barcodes", value = 5, min = 0.01, step = 1) %>% col_3(),
+        numericInput(ns("in_num_sepwd_genes"), label = "Sepwd: Genes", value = 0.5, min = 0.01, step = 1) %>% col_3(),
+        numericInput(ns("in_num_sepwd_samples"), label = "Sepwd: Samples", value = 0.25, min = 0.01, step = 1) %>% col_3()
+      ),
+      
+      moduleDownloadPlotUI(id = ns("mod_download_plot"))
+      #mod_select_maf_column_value_ui(id = ns("mod_select_clinical_feature_value")),
+    )
+  )
+}
+
+#' plot_gistic_oncoplot Server Functions
+#'
+#' @noRd 
+mod_plot_gistic_oncoplot_server <- function(id, gistic, maf){
+  utilitybeltshiny::assert_reactive(maf)
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    
+    
+    selected_clinical_feature <- mod_select_maf_clinical_data_column_server(id = "mod_select_clinical_feature", maf=maf, forced_to_pick_at_least_1 = FALSE)
+    
+    plot_gistic_oncoplot <- reactive({
+      function(){
+        maftools::gisticOncoPlot(
+          gistic = gistic(), 
+          clinicalData = maftools::getClinicalData(x = maf()), 
+          clinicalFeatures = selected_clinical_feature(),
+          showTumorSampleBarcodes = input$in_check_show_tsb,
+          sortByAnnotation = input$in_check_sort_by_annotation, 
+          removeNonAltered = input$in_check_remove_nonaltered_samples, 
+          SampleNamefontSize = input$in_num_fontsize_samplename,
+          fontSize = input$in_num_fontsize_cytobands,
+          legendFontSize = input$in_num_fontsize_legnend,
+          annotationFontSize = input$in_num_fontsize_annotations,
+          gene_mar = input$in_num_mar_gene, 
+          barcode_mar = input$in_num_mar_barcodes, 
+          sepwd_genes = input$in_num_sepwd_genes, 
+          sepwd_samples = input$in_num_sepwd_samples
+        )
+      }
+    })
+    
+    output$out_plot_gistic_oncoplot <- renderPlot({
+      plot_gistic_oncoplot()();
+      #Add download
+      #, clinicalData = maftools::getClinicalData(x = maf()), clinicalFeatures = 'FAB_classification', sortByAnnotation = TRUE, top = 10)  
+    })
+      moduleDownloadPlotServer(id = "mod_download_plot", session_parent = session, plotOutputId = "out_plot_gistic_oncoplot", plotting_function = plot_gistic_oncoplot(), default_filename = "GISTIC_oncoplot")
+  })
+}
+
+## To be copied in the UI
+# mod_plot_gistic_oncoplot_ui("plot_gistic_oncoplot_ui_1")
+
+## To be copied in the server
+# mod_plot_gistic_oncoplot_server("plot_gistic_oncoplot_ui_1")
--- R/mod_select_maf_column.R
+++ R/mod_select_maf_column.R
@@ -13,14 +13,14 @@ mod_select_maf_column_ui <- function(id, label = "Select Property"){
     shinyWidgets::pickerInput(inputId = ns("in_pick_maf_column"), label = label, choices = c(), options = shinyWidgets::pickerOptions(actionsBox = TRUE, liveSearch = TRUE))
   )
 }
-    
+
 #' select_maf_column Server Functions
 #'
 #' @noRd 
 mod_select_maf_column_server <- function(id, maf){
   utilitybeltshiny::assert_reactive(maf)
- 
-   moduleServer( id, function(input, output, session){
+  
+  moduleServer( id, function(input, output, session){
     ns <- session$ns
     
     maf_validated <- reactive({ validate(need(!is.null(maf()),message = "Loading ..." )); return(maf()) })
@@ -28,11 +28,11 @@ mod_select_maf_column_server <- function(id, maf){
     maf_columns.v <- reactive({
       maf_validated()@data %>% 
         colnames()
-      })
+    })
     
     observeEvent(maf_columns.v(), {
       shinyWidgets::updatePickerInput(session = session, inputId = "in_pick_maf_column", choices = maf_columns.v())
-      })
+    })
     
     in_pick_maf_column <- reactive({ 
       validate(need(!is.null(input[["in_pick_maf_column"]]),message = "Loading ..." )); return(input[["in_pick_maf_column"]]) })
@@ -40,9 +40,67 @@ mod_select_maf_column_server <- function(id, maf){
     return(in_pick_maf_column)
   })
 }
-    
+
 ## To be copied in the UI
 # mod_select_maf_column_ui("select_maf_column_ui_1")
+
+## To be copied in the server
+# mod_select_maf_column_server("select_maf_column_ui_1")
+
+
+#' select_maf_column UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_select_maf_clinical_data_column_ui <- function(id, label = "Select Property", multiple = FALSE){
+  ns <- NS(id)
+  tagList(
+    shinyWidgets::pickerInput(inputId = ns("in_pick_maf_column"), label = label, choices = c(), selected = character(0), multiple = multiple, options = shinyWidgets::pickerOptions(actionsBox = TRUE, liveSearch = TRUE))
+  )
+}
+
+#' select_maf_column Server Functions
+#'
+#' @noRd 
+mod_select_maf_clinical_data_column_server <- function(id, maf, forced_to_pick_at_least_1=TRUE){
+  utilitybeltshiny::assert_reactive(maf)
+  
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
     
+    maf_columns.v <- reactive({
+      validate(need(!is.null(maf()),message = "Loading ..." ));
+      maf() %>% maftools::getClinicalData() %>% 
+        colnames()
+    })
+    
+    observeEvent(maf_columns.v(), {
+      shinyWidgets::updatePickerInput(session = session, inputId = "in_pick_maf_column", choices = maf_columns.v(), selected = character(0))
+    })
+    
+    in_pick_maf_column <- reactive({ 
+      if(forced_to_pick_at_least_1){
+        validate(need(!is.null(input[["in_pick_maf_column"]]),message = "Please Select a Clinical Feature ..." ))
+      }
+      else{
+        if(is.null(input[["in_pick_maf_column"]]))
+          return(NULL)
+      }
+      
+      return(input[["in_pick_maf_column"]]) 
+    })
+    
+    return(in_pick_maf_column)
+  })
+}
+
+## To be copied in the UI
+# mod_select_maf_column_ui("select_maf_column_ui_1")
+
 ## To be copied in the server
 # mod_select_maf_column_server("select_maf_column_ui_1")
