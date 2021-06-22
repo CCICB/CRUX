b751fc608a37b36cc57d890db37b28001e2537fb
selkamand <sam.elkamand@gmail.com>
2021-06-23 06:05:28 +1000

    small improvements to oncoplot UI

--- R/mod_plot_oncoplots.R
+++ R/mod_plot_oncoplots.R
@@ -14,13 +14,14 @@ mod_plot_oncoplot_ui <- function(id){
                           conditionalPanel(condition = "input.in_checkbox_show_sample_names", ns=ns, numericInput(inputId = ns("in_num_fontsize_sample"), label = "Fontsize: Sample Names", min=0, step = 0.1, value = 1.0)) %>% col_2(),
                           col_8()
                         ),
+                        hr(),
                         fluidRow(
                           numericInput(inputId = ns("in_num_topn"), label = "Genes to plot", min=1, step = 1, value = 20)  %>% col_3(),
                           mod_select_genes_ui(id = ns("mod_select_genes_to_ignore"), label = "Genes to ignore", multiple = TRUE) %>% col_3(),
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_show_sample_plot"), label = "Show sample barplot", value = TRUE) %>% col_3(style="margin-top: 24px"),
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_show_gene_plot"), label = "Show gene barplot", value = TRUE) %>% col_3(style="margin-top: 24px"),
                         ),
-                        
+                        hr(),
                         fluidRow(
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_show_titv_plot"), label = "Show TiTv barplot", value = FALSE) %>% col_3(style="margin-top: 24px"),
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_draw_outline_plot"), label = "Draw outline", value = FALSE) %>% col_3(style="margin-top: 24px"),
@@ -28,29 +29,30 @@ mod_plot_oncoplot_ui <- function(id){
                           numericInput(inputId = ns("in_num_sepwd_samples"), label = "Size of lines separating samples", min=0.01, step = 0.1, value = 0.25)  %>% col_3()
                         ),
                         
-                        
+                        hr(),
                         
                         fluidRow(
-                          numericInput(inputId = ns("in_num_fontsize"), label = "Fontsize", min=0, step = 0.1, value = 0.8) %>% col_3(),
+                          numericInput(inputId = ns("in_num_fontsize"), label = "Fontsize", min=0, step = 0.1, value = 1.2) %>% col_3(),
                           numericInput(inputId = ns("in_num_fontsize_title"), label = "Fontsize: Title", min=0, step = 0.1, value = 1.5) %>% col_3(),
-                          numericInput(inputId = ns("in_num_fontsize_legend"), label = "Fontsize: Legend", min=0, step = 0.1, value = 1.2) %>% col_3(),
+                          numericInput(inputId = ns("in_num_fontsize_legend"), label = "Fontsize: Legend", min=0, step = 0.1, value = 2) %>% col_3(),
                           numericInput(inputId = ns("in_num_fontsize_annotation"), label = "Fontsize: annotation", min=0, step = 0.1, value = 1.5) %>% col_3(),
                         ),
                         
                         fluidRow(
-                          numericInput(inputId = ns("in_num_mar_barcode"), label = "Margin: tumor_sample_barcodes", min=0, step = 1, value = 4)  %>% col_3(),
-                          numericInput(inputId = ns("in_num_mar_gene"), label = "Margin: genes", min=0, step = 1, value = 5) %>% col_3(),
+                          numericInput(inputId = ns("in_num_mar_barcode"), label = "Margin: Tumor Sample Barcodes", min=0, step = 1, value = 4)  %>% col_3(),
+                          numericInput(inputId = ns("in_num_mar_gene"), label = "Margin: genes", min=0, step = 1, value = 8) %>% col_3(),
                           numericInput(inputId = ns("in_num_legend_height"), label = "Legend height", min=0, step = 1, value = 4)  %>% col_3(),
-                          shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_show_title"), label = "Toggle title", value = TRUE) %>% col_3(style="margin-top: 24px"),
+                          shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_show_title"), label = "Show title", value = FALSE) %>% col_3(style="margin-top: 24px"),
                         ),
                         
                         
-                        
+                        hr(),
                         fluidRow(
                           moduleGetColumnNameUI(ns("mod_select_clinical_feature"))  %>% col_3(),
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_sort_by_annotation"), label = "Sort by annotation", value = FALSE) %>% col_3(style="margin-top: 24px"),
                           shinyWidgets::awesomeCheckbox(inputId = ns("in_checkbox_use_custom_genes"), label = "Custom genes", value = FALSE) %>% col_3(style="margin-top: 24px"),
-                          conditionalPanel(condition = "input.in_checkbox_use_custom_genes", ns=ns, uiOutput(outputId = ns("out_ui_genelist"))) %>% col_3(style="margin-top: 24px")
+                          conditionalPanel(condition = "input.in_checkbox_use_custom_genes", ns=ns, mod_select_genes_ui(id = ns("mod_select_custom_genelist"), label = "Genes to plot",multiple = TRUE)) %>% col_3()
+                          #conditionalPanel(condition = "input.in_checkbox_use_custom_genes", ns=ns, uiOutput(outputId = ns("out_ui_genelist"))) %>% col_3()
                         ),
                         moduleDownloadPlotUI(ns("mod_download"))
                         
@@ -69,12 +71,13 @@ mod_plot_oncoplot_server <- function(id, maf){
                  
                  clinical_data_selected <- moduleGetColumnNameServer(id = "mod_select_clinical_feature", named_data = clinicalData, label = "Clinical Feature", multiple_selectable = T)
                  
-                 output$out_ui_genelist <- renderUI({ shinyWidgets::pickerInput( inputId = session$ns("in_pick_gene"), label = "Gene", choices = c(maf()@data$Hugo_Symbol) %>% sort %>% unique(), options = shinyWidgets::pickerOptions(liveSearch = T, actionsBox = T), multiple = T) })
+                 custom_genelist <- mod_select_genes_server("mod_select_custom_genelist", maf = maf)
+                 #output$out_ui_genelist <- renderUI({ shinyWidgets::pickerInput( inputId = session$ns("in_pick_gene"), label = "Gene", choices = c(maf()@data$Hugo_Symbol) %>% sort %>% unique(), options = shinyWidgets::pickerOptions(liveSearch = T, actionsBox = T), multiple = T) })
                  
                  genes <- reactive({ 
                    if (input$in_checkbox_use_custom_genes){
-                     validate(need(expr = !is.null(input$in_pick_gene), message = "Please select genes from the dropdown menu"))
-                     return(input$in_pick_gene) 
+                     validate(need(expr = !is.null(custom_genelist()), message = "Please select genes from the dropdown menu"))
+                     return(custom_genelist()) 
                    }
                    else
                      return(NULL)
