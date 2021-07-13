538b8106e39309e74382f79c3a92286e4eb7cbe9
selkamand <sam.elkamand@gmail.com>
2021-07-13 10:25:12 +1000

    Minor changes to description and lockckfile

--- R/data.R
+++ R/data.R
@@ -2,7 +2,6 @@
 #'
 #' A dataset describing the metadata of external tools used in the cancer space
 #' diamonds.
-#'
 #' @format A data frame with 3 rows and 10 variables:
 #' \describe{
 #' \item{external_tools_df} {the dataframe to add tool metadata to. By default, will create and return a new dataframe}
@@ -15,8 +14,7 @@
 #' \item{website}{url of tool (string)}
 #' \item{doi}{publicatoin doi (string)}
 #' \item{requires_maf_export}{does the tool require a maf to be exported in some other form (flag)}
-#' \item{maf_conversion_function}{only relevent if requires_maf_export == true. A function that takes a MAF object (first argument), and a filepath (second argument) and writes a file to that filepath. The idea is that said file can then be used as input to the specified tool.}
+#' \item{maf_conversion_function}{only relevent if requires_maf_export == true. A function that takes a MAF object (first argument), and a filepath (second argument) and writes a file to that filepath. The idea is that said file can then be used as input to the specified tool}
 #' }
 #' @source CRUX::external_tools_load_all_tools()
 "external_tool_metadata"
-
