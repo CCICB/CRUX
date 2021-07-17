23b36763d763afbe452a6bfa53e6791eb617ef58
= <sam.elkamand@gmail.com>
2021-07-17 14:36:12 +1000

    shinyApp options can now be set at runtime using options paramater of runApp

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,10 @@
 #' Run the Shiny Application
 #'
+#' @param options A list of  options to be passed to shinyApp. 
+#' Common options include launch.browser & port. 
+#' You must always set ALL options you care about in one go. 
+#' E.g. if you use options=list(port=3838) we cannot guarantee the launch.browser option will remain true. 
+#' If multiple options are important to you, force-set them all, e.g. \strong{options(list(launch.browser=TRUE, port = 3838))}
 #' @param ... A series of options to be used inside the app.
 #'
 #'
@@ -7,13 +12,14 @@
 #' @importFrom golem with_golem_options
 #' @export
 run_app <- function(
+  options = list(launch.browser = TRUE), # Passing options via ... seems to be failing. This options parameter lets us force-set shiny options while using sensible defaults
   ...
 ) {
   golem::with_golem_options(
     app = shiny::shinyApp(
       ui = app_ui, 
       server = app_server,
-      options = list(launch.browser = TRUE) #Hardcoding important options. Passing via `...` seems to be failing for some reason
+      options = options # Directly feed options to shinyApp. Passing via `...` doesn't seem to work for me
     ), 
     golem_opts = list(...)
   )
