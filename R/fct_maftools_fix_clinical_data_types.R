52686a3c06bbbb603dd19401266336ad55524e3e
selkamand <sam.elkamand@gmail.com>
2023-11-10 23:25:46 +1100

    feat: added brca subtype clinical feature

--- R/fct_maftools_fix_clinical_data_types.R
+++ R/fct_maftools_fix_clinical_data_types.R
@@ -20,3 +20,37 @@ maftools_fix_clinical_data_types <- function(maf){
   return(maf)
 }
 
+
+
+#' add BRCA subtype clinical feature
+#'
+#' Checks if maf is BRCA and contains cols required to classify as Triple Negative / Not Triple Negative.
+#' If so, adds a 'subtype' column describing whether its triple negative or not triple negative
+#'
+#' @param maf maftools maf object
+#'
+#' @return  maf object
+#'
+maftools_add_brca_subtype <- function(maf){
+  
+  clinical_feature_names <- colnames(maf@clinical.data)
+  
+  her2 = "lab_proc_her2_neu_immunohistochemistry_receptor_status"
+  #her2 = "lab_procedure_her2_neu_in_situ_hybrid_outcome_type"
+  progesterone = "breast_carcinoma_progesterone_receptor_status"
+  estrogen = "breast_carcinoma_estrogen_receptor_status"
+  
+  
+    if(all(c(her2, progesterone, estrogen) %in% clinical_feature_names)){
+      her2_status <- maf@clinical.data[[her2]]
+      pr_status <- maf@clinical.data[[progesterone]]
+      er_status <- maf@clinical.data[[estrogen]]
+      
+      maf@clinical.data[['subtype']] <- dplyr::case_when(
+        her2_status == "Negative" &  pr_status == "Negative" & er_status == "Negative" ~ "Triple Negative",
+        her2_status == 'Positive' | pr_status == "Positive" | er_status == "Positive" ~ "Not Triple Negative",
+        TRUE ~ "Ambiguous" # If any of the recepter/her2 statuses are indeterminate or equivocal
+      )
+    }
+  return(maf)
+}
